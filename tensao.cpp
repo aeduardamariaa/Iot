c275c7ffeb7a9f8894f817179308ed89ce0f15d1
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2023-11-22 13:55:37 -0300

    Update tensaoAnalogico.cpp

--- tensao.cpp
+++ tensao.cpp
@@ -1,21 +1,26 @@
+#include<Arduino.h>
+
+//pino
 const uint8_t PIN_INPUT = 26;
 
-const uint16_t  ADC_MAX = 4095;
-const float VCC = 3.3;
+//constatnte
+const uint32_t  ADC_MAX = 4095;
+const float VREF = 3.3f;
 
 void setup()
 {
   pinMode(PIN_INPUT, INPUT);
 
-  Serial.begin(115200);
+  Serial.begin(115200);//inicializa comunicação via USB, e passa o valor da velocidade da comunicação 
 }
 
 void loop()
 {
-  int input = analogRead(PIN_INPUT);
-  float tensao = static_cast<float>(input) input * VCC / ADC_MAX; //divisão de int por int retorna inteiro
+  uint16_t input_value = analogRead(PIN_INPUT);
 
-  Serial.print(tensao); // manda string
-  Serial.write(); // manda 1 char
+  float tensao = static_cast<float>(input_value) * VREF / static_cast<float>(ADC_MAX); //divisão de int por int retorna inteiro
+  
+  // Serial.print(); // manda string
+  // Serial.write(); // manda 1 char
   Serial.println(); // manda que pula de linha 
 }
